7452047a257d34a7500716adb5f6887e9cbc6ab4
Omar Saied <74738267+omarsa3ied@users.noreply.github.com>
2022-04-27 02:08:11 +0200

    ..

--- Data_Structure_Project/Preparation.h
+++ Data_Structure_Project/Preparation.h
@@ -1,14 +1,30 @@
 #pragma once
 #include<iostream>
+#pragma once
 #include "Events.h"
 #include "company.h"
 #include "Cargo.h"
 #include "Time.h"
+#include "QueueADT.h"
+#include "PriorityQ.h"
+#include "LinkedQueue.h"
 using namespace std;
 class Preparation :Events
 {
 public:
-	void Execute()
+	Time Execute()
+	{}
+	Time Execute(LinkedQueue<Cargo>waiting_list,Time P_T, Time L_T , cargoType c_t , int dist, int cost, Time event_time)
 	{
+		Cargo c;
+		c.setCost(cost);
+		c.setDeliveryDistance(dist);
+		c.setLoadAndUnloadTime(L_T);
+		c.setPreparationTime(P_T);
+		c.setTypeOfCargo(c_t);
+		c.setID(waiting_list.QueueCount(waiting_list) + 1);
+		waiting_list.peek(c);
+		return event_time;
 	}
+
 };
